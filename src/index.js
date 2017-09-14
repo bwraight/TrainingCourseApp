3ff5c37a56bc1a7192278cdcfd6dd10e5f1afe7e
bwraight <b.r.wraight@gmail.com>
2017-09-14 21:35:52 +0100

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
-/* eslint-disable import/default */
-
 import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import configureStore, { history } from './store/configureStore';
-import Root from './components/Root';
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-require('./favicon.ico'); // Tell webpack to load favicon.ico
-const store = configureStore();
+import ReactDOM from 'react-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
 
-render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
-  document.getElementById('app')
-);
+// Styles
+// Import Font Awesome Icons Set
+import 'font-awesome/css/font-awesome.min.css';
+  // Import Simple Line Icons Set
+import 'simple-line-icons/css/simple-line-icons.css';
+// Import Main styles for this application
+import '../scss/style.scss'
 
-if (module.hot) {
-  module.hot.accept('./components/Root', () => {
-    const NewRoot = require('./components/Root').default;
-    render(
-      <AppContainer>
-        <NewRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById('app')
-    );
-  });
-}
+// Containers
+import Full from './containers/Full/'
+
+const history = createBrowserHistory();
+
+ReactDOM.render((
+  <HashRouter history={history}>
+    <Switch>
+      <Route path="/" name="Home" component={Full}/>
+    </Switch>
+  </HashRouter>
+), document.getElementById('root'));
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+const routes = {
+  '/': 'Home',
+  '/dashboard': 'Dashboard',
+  '/courses': 'Course',
 
-import App from './components/App';
-import HomePage from './components/HomePage';
-import FuelSavingsPage from './containers/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
-import AboutPage from './components/AboutPage';
-import NotFoundPage from './components/NotFoundPage';
-
-export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={HomePage}/>
-    <Route path="fuel-savings" component={FuelSavingsPage}/>
-    <Route path="about" component={AboutPage}/>
-    <Route path="*" component={NotFoundPage}/>
-  </Route>
-);
+};
+export default routes;
